e718daf950d5f209ed7d57d1c28b664138e9293d
David Munyiri <davidmumyiri2019@gmail.com>
2024-03-14 00:31:01 +0300

    did a minor change

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     port: process.env.PORT || 5000,
     jwtSecret: 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxMDMzNzA1OCwiaWF0IjoxNzEwMzM3MDU4fQ.DacGC0zELvQ5mRGO46iR3Au95ggv5xPi9icQCd3QXF8',
     db: {
-      url: 'mongodb+srv://meshakotieno343:c6w83r3QNTXKXDqy@cluster0.cgm5bnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+      url: 'mongodb+srv://munyiricluster.erg9yiq@cluster0.cgm5bnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     },
   };
   
